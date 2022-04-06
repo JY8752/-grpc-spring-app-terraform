a089a20e59d16369d91f0c10d28c4ac724a0212b
JY8752 <j.y.8752@gmail.com>
2022-04-06 20:38:37 +0900

    こうか

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
   region     = "ap-northeast-1"
 }
 
