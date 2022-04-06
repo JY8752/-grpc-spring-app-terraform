2cbb73717321db87ea3e0551ceb0601be72d7e2b
JY8752 <j.y.8752@gmail.com>
2022-04-06 20:44:04 +0900

    やっぱproviderブロック自体いらないのか

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-variable "aws_access_key_id" {}
-variable "aws_secret_access_key" {}
-provider "aws" {
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-  region     = "ap-northeast-1"
-}
-
 module "vpc" {
   source = "./module/vpc"
   input  = local.input
