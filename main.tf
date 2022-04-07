b80aeff46675cc45347a1de69274a7f3a812a00c
JY8752 <j.y.8752@gmail.com>
2022-04-07 09:16:23 +0900

    むむむ

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
-variable "aws_access_key" {}
-variable "secret_key" {}
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.secret_key
+  region = "ap-northeast1"
 }
 module "vpc" {
   source = "./module/vpc"
